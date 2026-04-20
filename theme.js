584a896913af80835422890b5d695add8d675523
Davinder <singhdavinderjeet564@gmail.com>
2026-04-20 18:59:13 +0530

    added a tooltip to theme change button, added styles for outline for theme button

--- theme.js
+++ theme.js
@@ -1,10 +1,19 @@
 const body = document.body;
 const themeChangeBtn = document.querySelector('.theme-change-btn');
 
-localStorage.getItem('theme') === 'dark' ? body.classList.add('darkmode') : body.classList.remove('darkmode');
+const theme =  localStorage.getItem('theme');
+
+if (theme === 'dark') {
+    body.classList.add('darkmode');
+    body.classList.remove('lightmode');
+} else {
+    body.classList.add('lightmode');
+    body.classList.remove('darkmode');
+}
 
 themeChangeBtn.addEventListener('click', () => {
     themeChangeBtn.classList.toggle('clicked');
     body.classList.toggle('darkmode');
+    body.classList.toggle('lightmode');
     localStorage.setItem('theme', body.classList.contains('darkmode') ? 'dark' : 'light');
 });
